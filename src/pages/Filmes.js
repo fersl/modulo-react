9bacb11a12a18e3f6a38e3301e685f793d56642a
Fernanda Soares <32881250+fersl@users.noreply.github.com>
2023-08-23 12:21:08 -0300

    projeto - passo 2.1
    Fazendo a conexão com a API TMDB utilizando 'fetch' e utilizando 'axios'.

--- src/pages/Filmes.js
+++ src/pages/Filmes.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+import { API_KEY, URL_FILMES } from "../apiConfig"
+import axios from "axios"
+
 function Filmes() {
-    return(
+
+    const [filmes, setFilmes] = useState([])
+
+    // useEffect( função, array de dependências )
+    // useEffect( ()=>{}, [] )
+
+    // Use apenas uma das opções abaixo:
+    // 1 - CONEXÃO COM FETCH
+    useEffect(() => {
+        fetch(`${URL_FILMES}/popular${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log("retorno do fetch:", data)
+            setFilmes(data.results)
+        })
+    }, [])
+
+    // 2 - CONEXÃO COM AXIOS
+    useEffect(() => {
+        axios.get(`${URL_FILMES}/popular${API_KEY}`)
+        .then(res => {
+            console.log("retorno do axios:", res)
+            setFilmes(res.data.results)
+        })
+    }, [])
+
+    console.log("filmes:", filmes);
+
+    return (
         <h1>Página de Filmes</h1>
     )
 }
--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react"
+import { API_KEY, URL_SERIES } from "../apiConfig"
+import axios from "axios"
+
 function Series() {
+
+    const [series, setSeries] = useState([])
+
+    // 1 - CONEXÃO COM FETCH
+    useEffect(() => {
+        fetch(`${URL_SERIES}/popular${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log("retorno do fetch:", data)
+            setSeries(data.results)
+        })
+    }, [])
+
+    // 2 - CONEXÃO COM AXIOS
+    useEffect(() => {
+        axios.get(`${URL_SERIES}/popular${API_KEY}`)
+        .then(res => {
+            console.log("retorno do axios:", res)
+            setSeries(res.data.results)
+        })
+    }, [])
+
+    console.log("series:", series)
+
     return(
         <h1>Página de Séries</h1>
     )
